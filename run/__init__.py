0bfe127470238047df64682e59678f17c45d8d97
JesusChuz <45716413+JesusChuz@users.noreply.github.com>
2022-10-12 15:23:17 -0600

    Update __init__.py

--- run/__init__.py
+++ run/__init__.py
@@ -3,7 +3,7 @@ import azure.functions as func
 
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
-    logging.info('Python HTTP trigger function processed a request.')
+    logging.info('Pythoon HTTP trigger function processed a request.')
 
     name = req.params.get('name')
     if not name:
